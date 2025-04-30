aec37c4b0a5204880802a64e775fbe70f2b403eb
DaviLamarca <davidiscorddev@gmail.com>
2025-04-29 23:19:16 -0300

    atualização validação senha

--- server.js
+++ server.js
@@ -1,45 +1,36 @@
-import http from 'http'
-import url from 'url'
-import fs from 'fs'
+import http from 'http';
+import url from 'url';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
     const parse = url.parse(req.url, true);
     const method = req.method;
     const pathname = parse.pathname;
 
-    let URLBase = "/"
-
     if (method === "GET" && pathname === "/ping") {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        let numeros = []
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        let numeros = [];
         for (let i = 0; i < 15; i++) {
-            numeros += i + 1
+            numeros.push(i + 1);
         }
-        res.end(JSON.stringify({ msg: "Tudo certo, serve de pé", objeto: numeros }))
-        return
+        res.end(JSON.stringify({ msg: "Tudo certo, servidor de pé", objeto: numeros }));
+        return;
     }
 
     if (method === "POST" && pathname === "/cadastro") {
-        let bodyPessoa = ""
-        req.on('data', chunk => {
-            bodyPessoa += chunk.toString()
-        });
-
+        let bodyPessoa = "";
+        req.on('data', chunk => { bodyPessoa += chunk.toString(); });
         req.on('end', () => {
-            let pessoa = JSON.parse(bodyPessoa)
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(pessoa))
+            let pessoa = JSON.parse(bodyPessoa);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(pessoa));
         });
-        return
+        return;
     }
 
     if (method === "POST" && pathname === "/ajuda") {
         let bodyPessoa = "";
-
-        req.on('data', chunk => {
-            bodyPessoa += chunk.toString();
-        });
-
+        req.on('data', chunk => { bodyPessoa += chunk.toString(); });
         req.on('end', () => {
             let pessoa;
             try {
@@ -65,31 +56,35 @@ const server = http.createServer((req, res) => {
                 erro = true;
                 formErro.senha = "Sem senha não salva, parceiro";
             }
-
+            if (pessoa.senha.length <= 5) {
+                erro = true
+                formErro.senha = "A senha tem que ter mais que 5 caracteres"
+            }
             if (erro) {
                 res.writeHead(400, { 'Content-Type': 'application/json' });
                 res.end(JSON.stringify(formErro));
                 return;
             }
-            fs.appendFile("Pessoas.json", pessoa, (err) => {
+
+            fs.appendFile("Pessoas.json", JSON.stringify(pessoa) + "\n", (err) => {
                 if (err) {
                     console.log(err);
-                    return
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ erro: "Erro ao salvar pessoa" }));
+                    return;
                 }
 
-            })
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ pessoa }));
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ pessoa }));
+            });
         });
-
         return;
     }
 
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ msg: "Rota não encontrada." }));
+});
 
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ msg: "Rota não encontrada." }))
-    return
-})
 server.listen(3000, () => {
-    console.log("servidor de pé, esperando requisições")
-})
+    console.log("Servidor de pé, esperando requisições");
+});
